8bb5792111e55d1fdbf760de25ef04990159838e
jKostet <juho.kostet@helsinki.fi>
2017-01-08 20:09:12 +0200

    Show correct Cursor cound and price

--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-
-
 // Keyboard clicks
 var lines = 0;
 
@@ -11,22 +9,28 @@ function keyboardClick(n) {
 // Cursors
 var cursors = 0;
 var cursorEfficiency = 0.5;
+var cursorCost = 10;
 
 function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
     if(lines >= cursorCost){                                     //checks that the player can afford the cursor
       cursors = cursors + 1;                                     //increases number of cursors
-	    lines  -= cursorCost;                                      //removes the cookies spent
+      lines -= cursorCost;                                       //removes the LinesOfCode spent
+      cursorCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
+
+      updateStats();
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
-    updateStats();
 };
 
 function updateStats() {
   document.getElementById('cursors').innerHTML = cursors;       //updates the number of cursors for the user
   document.getElementById('lineCount').innerHTML = lines;       //updates the number of cookies for the user
-  document.getElementById('cursorCost').innerHTML = nextCost;   //updates the cursor cost for the user
+  //document.getElementById('cursorCost').innerHTML = nextCost;   //updates the cursor cost for the user
+  document.getElementById('cursorCost').innerHTML = cursorCost;   //updates the cursor cost for the user
 }
+
+// at page load
+updateStats();
+
 
 // Game
 window.setInterval(function(){
