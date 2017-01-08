70d32c87687adf023cf030adf6f2c0dca9adf8d7
jKostet <juho.kostet@helsinki.fi>
2017-01-08 19:08:59 +0200

    Add RESET button to reset game

--- game.js
+++ game.js
@@ -17,15 +17,26 @@ function buyCursor(){
     if(lines >= cursorCost){                                     //checks that the player can afford the cursor
       cursors = cursors + 1;                                     //increases number of cursors
 	    lines  -= cursorCost;                                      //removes the cookies spent
-      document.getElementById('cursors').innerHTML = cursors;    //updates the number of cursors for the user
-      document.getElementById('lineCount').innerHTML = lines;  //updates the number of cookies for the user
     };
     var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    updateCursorStats();
 };
+
+function updateCursorStats() {
+  document.getElementById('cursors').innerHTML = cursors;       //updates the number of cursors for the user
+  document.getElementById('lineCount').innerHTML = lines;       //updates the number of cookies for the user
+  document.getElementById('cursorCost').innerHTML = nextCost;   //updates the cursor cost for the user
+}
 
 // Game
 window.setInterval(function(){
   var c = cursors * cursorEfficiency;
   keyboardClick(c)
 }, 1000);
+
+// Dev
+function reset() {
+  lines = 0;
+  cursors = 0;
+  updateCursorStats();
+}
